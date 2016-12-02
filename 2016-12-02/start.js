58200fcf424d5dea907e98820801992677934cbb
Philip Walton <philip@philipwalton.com>
2016-12-02 17:02:14 +0100

    Deploy talks

--- 2016-12-02/start.js
+++ 2016-12-02/start.js
@@ -1,24 +1,28 @@
-const babelify = require('babelify');
-const browserify = require('browserify');
+require('shelljs/global');
+
 const connect = require('connect');
 const fs = require('fs');
 const glob = require('glob');
 const serveStatic = require('serve-static');
 
+
 const entries = glob.sync('demos/[1-9]/index.js');
 
 fs.watch('demos', {recursive: true}, (eventType, filename) => {
-  if (!filename.match(/bundle\.js$/)) buildSources();
+  if (!filename.match(/bundle\.js$/)) {
+    console.log('Change detected, rebuilding source files...');
+    buildSources();
+  }
 });
 
 const buildSources = () => {
   for (const entry of entries) {
-    console.log(`Compiling ${entry}`);
-    browserify(entry, {debug: true}).transform(babelify, {
-      presets: ['es2015', 'stage-2']
-    })
-    .bundle()
-    .pipe(fs.createWriteStream(entry.replace(/\.js$/, '.bundle.js')));
+    const browserify = 'node_modules/.bin/browserify';
+    const babelify = 'babelify --presets [ stage-2 es2015 ]';
+    const output = entry.replace(/\.js$/, '.bundle.js');
+
+    exec(`${browserify} ${entry} -t [ ${babelify} ] | uglifyjs > ${output}`,
+        () => console.log(`Compiled: ${entry}`));
   }
 }
 
