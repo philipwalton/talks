b39e339afdde759d2dce54da5e9d67b037bc3098
Philip Walton <philip@philipwalton.com>
2017-06-15 01:57:46 +0200

    Add latest changes

--- 2017-06-15/demos/8/circle.js
+++ 2017-06-15/demos/8/circle.js
@@ -2,17 +2,13 @@ registerPaint('circle', class {
   static get inputProperties() {
     return ['--circle-color'];
   }
-  paint(ctx, geom, styleMap) {
-    // Set the color from the customm property.
-    ctx.fillStyle = styleMap.get('--circle-color');
-
-    // Determine the center point and radius.
+  paint(ctx, geom, props) {
     const x = geom.width / 2;
     const y = geom.height / 2;
     const radius = Math.min(x, y);
+    const color = props.get('--circle-color').toString();
 
-    // Draw the circle \o/
-    ctx.beginPath();
+    ctx.fillStyle = color;
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
     ctx.fill();
   }
